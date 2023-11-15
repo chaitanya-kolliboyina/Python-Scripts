4ddd40c88290b081e468f9c91e2931552f8ad081
Chaitanya-Endurance <kvschaitanya17@gmail.com>
2023-11-15 19:11:21 +0530

    New Programs

--- Basic_math_programs.py
+++ Basic_math_programs.py
@@ -5,11 +5,30 @@
 
 #Check palindrome
 
-n = input("Enter input : ")
-def check_palindrome(n:str )->bool :
-    rev = n[::-1]
-    if n == rev :
-        return True
+# n = input("Enter input : ")
+# def check_palindrome(n:str )->bool :
+#     rev = n[::-1]
+#     if n == rev :
+#         return True
+#     else :
+#         return False
+# print(check_palindrome(n))
+
+# Check Prime number 
+
+n = int(input("Enter input : "))
+def check_prime(n: int) ->bool :
+
+    for i in range(2,n//2):
+        if n%i == 0 :
+            return False
     else :
-        return False
-print(check_palindrome(n))
+        return True
+        
+    
+print(check_prime(n))
+            
+
+
+
+
