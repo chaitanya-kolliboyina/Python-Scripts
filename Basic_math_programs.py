b1a4555773e19c7c7c1fe8b884b522b756e88049
Chaitanya-Endurance <kvschaitanya17@gmail.com>
2023-11-21 08:17:56 +0530

    New problem

--- Basic_math_programs.py
+++ Basic_math_programs.py
@@ -1,9 +1,9 @@
-# Reverse of a number
+##### Reverse of a number
 
 # n = input()
 # print(n[::-1])
 
-#Check palindrome
+##### Check palindrome
 
 # n = input("Enter input : ")
 # def check_palindrome(n:str )->bool :
@@ -14,20 +14,31 @@
 #         return False
 # print(check_palindrome(n))
 
-# Check Prime number 
+##### Check Prime number 
+
+# n = int(input("Enter input : "))
+# def check_prime(n: int) ->bool :
+
+#     for i in range(2,n//2):
+#         if n%i == 0 :
+#             return False                           # IF n is checked untill square root of n, time complecity reduces to sqrt(n) - Optimal solution
+#     else :
+#         return True
+           
+# print(check_prime(n))
+            
+#### Print divisors of given number 
 
 n = int(input("Enter input : "))
-def check_prime(n: int) ->bool :
+def print_divisors(n:int) ->list:
+    ls = [] 
+    for i in range(1,n+1):
+        if (n%i == 0):
+            ls.append(i)
+    return ls
 
-    for i in range(2,n//2):
-        if n%i == 0 :
-            return False
-    else :
-        return True
-        
-    
-print(check_prime(n))
-            
+print(print_divisors(n))
+
 
 
 
