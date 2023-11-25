fa20e8c92eaa780b897a7d74f5993323a405cb19
Chaitanya-Endurance <kvschaitanya17@gmail.com>
2023-11-25 16:15:28 +0530

    Excercise

--- P4E_Excercises/Chapter_6.py
+++ P4E_Excercises/Chapter_6.py
@@ -21,5 +21,15 @@
 # Write an invocation that counts the number of times the letter a occurs
 # in “banana”.
 
-string = 'banana'
-print(string.count('a'))
+# string = 'banana'
+# print(string.count('a'))
+
+# Exercise 5: Take the following Python code that stores a string:
+# str = 'X-DSPAM-Confidence:0.8475'
+# Use find and string slicing to extract the portion of the string after the
+# colon character and then use the float function to convert the extracted
+# string into a floating point number.
+
+string = 'X-DSPAM-Confidence:0.8475'
+col = string.find(':')
+print(float(string[col+1:]))
